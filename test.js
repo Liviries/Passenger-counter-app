fb76fc88ab98865f7ac01bab67cecb8e2a81dc75
Liviries <liviriez@gmail.com>
2024-12-17 13:46:55 +0200

    first steps

---
+++ test.js
@@ -0,0 +1,80 @@
+let clwn = 'jstr', jsstr = 'jstr2';
+let eshk;
+eshk = 'UwU';
+const NY24 = '31.12.2024';
+
+let fF = 'starwb';
+let fI = "chocolate";
+let FPL = JSC;
+
+let nOD= 12;
+let pi = 3.14;
+
+let vLN = 327676765765657657567756567765567n;
+
+let lC = true;
+
+let fC
+
+FPL = null;
+
+const uK = Symbol();
+
+let course = {
+    name: 'course',
+    hours: 4,
+    AName: 'Steven',
+    ASurname: 'Garcia'
+
+
+};
+
+console.log(eshk);
+console.log(jsstr);
+console.log(clwn);
+console.log(NY24);
+console.log(fF);
+console.log(fI);
+console.log(FPL);
+console.log(nOD);
+console.log(pi);
+console.log(vLN);
+console.log(lC);
+console.log(fC);
+console.log(uK);
+console.log(course);
+console.log(typeof course);
+console.log(course.AName);
+console.log(course.ASurname);
+console.log(course['AName']);
+console.log(course['ASurname']);
+
+course.AName = 'Liviries';
+course['ASurname'] = 'Reld';
+
+console.log(course.AName);
+console.log(course['ASurname']);
+
+let time = 'hours';
+
+console.log(course[time]);
+
+
+let poducts = [pi, course.AName ,course.ASurname];
+console.log(poducts);
+console.log(course);
+console.log(typeof poducts);
+console.log(typeof course);
+console.log(course.AName.length);
+
+function vseDisainer(Duzhk, sDuzhk) {
+    console.log('Hi!' + Duzhk + sDuzhk);
+
+}
+
+vseDisainer(')))', '((');
+
+function Multiply(num1, num2) {
+    return num1 * num2;
+}
+console.log( Multiply (2, 2) );
