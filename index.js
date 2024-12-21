2f0346f2e7219be7478fad997ddba11982b918bc
Liviries <liviriez@gmail.com>
2024-12-21 21:20:28 +0200

    fully completed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-// document.getElementById("count-el").innerText = 5
+
+let countEl = document.getElementById("count-el");
 
 let peopleEnt = 0
 
 function increment() {
     peopleEnt = peopleEnt + 1
+    countEl.innerText = peopleEnt
     console.log(peopleEnt)
 }
