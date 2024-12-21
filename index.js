c9fd946f35fd8ad3bd5a385449dbb40a96a1688b
Liviries <liviriez@gmail.com>
2024-12-21 23:41:28 +0200

    Enhanced functionality, improved appearance

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 
-let countEl = document.getElementById("count-el");
+let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
 let peopleEnt = 0
 
 function increment() {
-    peopleEnt = peopleEnt + 1
-    countEl.innerText = peopleEnt
-    console.log(peopleEnt)
+    peopleEnt += 1
+    countEl.textContent = peopleEnt
+    console.log(peopleEnt);
+}
+
+function decrement() {
+    peopleEnt -= 1;
+    countEl.textContent = peopleEnt;
+    console.log(peopleEnt);
+}
+
+function save() {
+    let prevEntries = peopleEnt + " - "
+    saveEl.textContent += prevEntries;
+    countEl.textContent = 0
+    peopleEnt = 0
+    console.log(prevEntries);
 }
